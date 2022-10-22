ec7d684f8738424f1fa79df83495bdb083067aef
RolazarCoding <email@rolazarcoding.com>
2022-10-22 09:13:39 -0700

    Renamed folder

--- server/router/routes/home.js
+++
@@ -1,6 +0,0 @@
-module.exports = urlHome;
-
-function urlHome(_request, response) {
-  response.sendFile("index.html", { root: "public" });
-  // response.sendFile("home.html", { root: "public" });
-}
