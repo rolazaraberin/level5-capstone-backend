fab243f437c9109acc68b5081e5e63fdf6c81f7e
Rolazar Aberin <rolazaraberin@gmail.com>
2023-01-19 19:32:23 -0800

    Added error handling

--- src/controllers/account.ts
+++ src/controllers/account.ts
@@ -6,11 +6,20 @@ const account = { fetchInfo };
 export default account;
 
 async function fetchInfo(request: Request, response: Response) {
-  const { email, token } = request.body;
-  const userID = await authenticate.token(email, token);
-  const account = await getUserInfo(userID);
-  if (!account)
-    return response.status(401).send("ERROR: Cannot retrieve account");
+  try {
+    const { email, token } = request.body;
+    const userID = await authenticate.token(email, token);
+    const account = await getUserInfo(userID);
+    if (!account)
+      return response.status(401).send("ERROR: Cannot retrieve account");
+    response.status(200).send(account);
+  } catch (asyncError) {
+    const error = await asyncError;
+    debugger;
+    const message = error.message;
+    const code = error.code;
+    response.status(code).send(message);
+  }
 }
 
 async function getUserInfo(userID: number) {
