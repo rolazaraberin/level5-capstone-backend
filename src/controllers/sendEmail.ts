e422ecc13adb321aaf1e348c43219b70d8accf79
Rolazar Aberin <rolazaraberin@gmail.com>
2023-01-19 22:52:36 -0800

    Added email

---
+++ src/controllers/sendEmail.ts
@@ -0,0 +1,41 @@
+import sendGrid, { MailDataRequired } from "@sendgrid/mail";
+import dotenv from "dotenv";
+dotenv.config();
+
+const sendEmail = { signupConfirmation, deleteConfirmation };
+export default sendEmail;
+
+//CONNECT TO MAIL SERVICE
+sendGrid.setApiKey(process.env.sendGridApiKey as string);
+
+function signupConfirmation(email: string) {
+  const message: MailDataRequired = {
+    from: "rolazaraberin.test@gmail.com",
+    to: email,
+    subject: "Signup Confirmation",
+    text: `${email} has been signed up for Online Store`,
+    html: `<p>${email} has been signed up for Online Store</p>`,
+  };
+  send(message);
+}
+
+function deleteConfirmation(email: string) {
+  const message: MailDataRequired = {
+    from: "rolazaraberin.test@gmail.com",
+    to: email,
+    subject: "Delete Confirmation",
+    text: `${email} has been deleted from Online Store`,
+    html: `<p>${email} has been deleted from Online Store</p>`,
+  };
+  send(message);
+}
+
+async function send(message: MailDataRequired) {
+  try {
+    await sendGrid.send(message);
+  } catch (asyncError) {
+    const error = await asyncError;
+    console.log(error);
+    debugger;
+  }
+}
