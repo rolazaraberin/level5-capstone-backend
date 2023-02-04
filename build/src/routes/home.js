d0b95dc74c13f6eed462c2fd59cc5483080b1158
Rolazar Aberin <rolazaraberin@gmail.com>
2023-02-03 18:25:28 -0800

    build

--- build/src/routes/home.js
+++ build/src/routes/home.js
@@ -1,7 +1,1 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = urlHome;
-function urlHome(_request, response) {
-    response.sendFile("index.html", { root: "public" });
-    // response.sendFile("home.html", { root: "public" });
-}
+"use strict";function urlHome(_request,response){response.sendFile("index.html",{root:"public"})}Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=urlHome;
