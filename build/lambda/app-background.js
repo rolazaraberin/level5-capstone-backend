dbb31acb53f2b02f4421a0102dc2ccde204c2c7b
Rolazar Aberin <rolazaraberin@gmail.com>
2023-02-09 18:34:48 -0800

    build

--- build/lambda/app-background.js
+++
@@ -1,1 +0,0 @@
-"use strict";var __importDefault=this&&this.__importDefault||function(mod){return mod&&mod.__esModule?mod:{default:mod}};Object.defineProperty(exports,"__esModule",{value:!0}),exports.handler=void 0;const index_1=__importDefault(require("../src/index")),serverless_http_1=__importDefault(require("serverless-http"));exports.handler=(0,serverless_http_1.default)(index_1.default);
