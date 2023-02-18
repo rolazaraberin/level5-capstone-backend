6ddde8f06c61b1cd0d2ee8fd54fdd61906d2ec52
Rolazar Aberin <rolazaraberin@gmail.com>
2023-02-18 15:23:49 -0800

    Updated types

--- src/models/types.ts
+++ src/models/types.ts
@@ -9,12 +9,22 @@ export interface Cart {
   itemsTable: string;
   totalQuantity?: number;
   totalPrice?: number;
+  items?: Item[];
+}
+
+export interface Item {
+  id: number;
+  itemID?: number;
+  name?: string;
+  description?: string;
+  price?: number;
+  quantity: number;
 }
 
 export interface User {
   id: number;
   email: string;
-  token: string;
-  password: string;
+  token?: string;
+  password?: string;
   cartID: number;
 }
